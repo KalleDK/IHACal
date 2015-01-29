ec270b76442345cf91c1e8926e94c78d50965653
Kalle Møller <github.com@k-moeller.dk>
2015-01-29 22:07:15 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ while read line
 do
 	./update.py $line
 	curl -T --user $2:$3 calendars/$line.ics http://icalx.com/public/KalleDK/ > /dev/null 2>&1
-	echo "$line | [![ICS](https://img.shields.io/badge/ICS-build-green.svg)](http://icalx.com/public/KalleDK/$line.ics) | [![HTML](https://img.shields.io/badge/HTML-build-green.svg)](http://icalx.com/public/KalleDK/$line)" >> Readme.md
+	echo "$line | [![ICS](https://img.shields.io/badge/ICS-build-green.svg)](http://icalx.com/public/KalleDK/$line.ics) | [![HTML](https://img.shields.io/badge/HTML-build-green.svg)](http://www.icalx.com/html/KalleDK/week.php?cal=$line)" >> Readme.md
 done < $1
 echo "" >> Readme.md
 echo "Kalle DK" >> Readme.md
