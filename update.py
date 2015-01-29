a4732329d1de148cfba3723712a11ea745d840a5
KalleDK <github.com@k-moeller.dk>
2015-01-29 15:40:16 +0100

    push

--- update.py
+++ update.py
@@ -62,13 +62,14 @@ for week in range(1, last_week+1):
 		if (event_state == 3 and match):
 			event_state = 4
 			print match.group(1)
-			event.add('summary', match.group(1))
+			#event.add('summary', match.group(1))
+			event.add('summary', 'flaf')
 
 		match = re.search("<td align='left' bgcolor='#FFFFFF'><font color='#000080'>(.*)</font>", line)
 		if (event_state == 4 and match):
 			event_state = 5
 			print match.group(1)
-			event.add('location', match.group(1))
+			#event.add('location', match.group(1))
 
 		match = re.search("<td align='right' bgcolor='#FFFFFF'><font color='#000080'>(.*)</font>", line)
 		if (event_state == 5 and match):
